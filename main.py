1e2b71efea6576276f8da85f2c2cb79ecf137c08
MonireBergs <monire86@hotmail.com>
2025-01-11 21:02:59 +0100

    Veckouppgift 1.1 + 1.2

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print("Hello world")
 print("This program was made by MonirehBergstrom.")
+
+biljettPris = 100  # biljettpris
+fickPengar = 200  # pengar på fickan
+print("Det blir " + str(fickPengar - biljettPris) + " kronor över.")
+z = 200 - 100 / 2
+print("Hälften är: " + str(z))
