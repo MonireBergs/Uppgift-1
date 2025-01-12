e8c95e4ae65ac586728eb8e125baea8b4dba5353
MonireBergs <monire86@hotmail.com>
2025-01-12 16:34:53 +0100

    Veckouppgift 1.1 + 1.2, 3.1a

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ print("This program was made by MonirehBergstrom.")
 biljettPris = 100  # biljettpris
 fickPengar = 200  # pengar på fickan
 print("Det blir " + str(fickPengar - biljettPris) + " kronor över.")
-z = 200 - 100 / 2
+z = (200 - 100) / 2
 print("Hälften är: " + str(z))
+
+#Vi har hittat tre sätt att skriva ut strängar.
+#Med konvertering tex str(variabel) med ,(komma) och med print(f...
+
+x = input("Skriv in ett heltal:")
+print(x)
+y = input ("skriv in ett heltal:")
+print(f"Summan av talen är: {int(x) + int(y)}")
