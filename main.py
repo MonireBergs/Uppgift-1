b4fbc6903f04f78a19486757761d0145c97a3b79
MonireBergs <monire86@hotmail.com>
2025-01-12 20:59:35 +0100

    Veckouppgift 1.1 + 1.2 + 2 + 3.1a+b, 3.2a.b

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ x = input("Skriv in ett heltal:")
 print(x)
 y = input ("skriv in ett heltal:")
 print(f"Summan av talen är: {int(x) + int(y)}")
+
+
+#uppgift 3.2.a
+jacka = 2000
+rea_procent = 50
+slut_pris = jacka * rea_procent / 100
+print ("jackan kostar: " + str(slut_pris) +"kronor med " +str(rea_procent )+"% rabatt")
+
+
+
+jacka = 2000
+rabatt = input("Vilken rabatsats är det på jackan?")
+rabattSumma = jacka * (int(rabatt)/100)
+slutSumma = jacka - rabattSumma
+print("Jackan kostar:" + str(slutSumma)+ "kronor med " + str(rabatt) + "% rabatt")
